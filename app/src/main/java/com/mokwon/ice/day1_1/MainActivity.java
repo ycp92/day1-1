31a9823379d52ffa2583672389a27f0c6540f859
ycp92 <chanouo@naver.com>
2015-09-07 11:06:32 +0900

    변수정의

--- app/src/main/java/com/mokwon/ice/day1_1/MainActivity.java
+++ app/src/main/java/com/mokwon/ice/day1_1/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     int a;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
